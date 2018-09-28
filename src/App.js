5c74d8a92ba977369b82ab31c34623587f15beaf
Maximilian Meyer <maximilian.meyer@br.de>
2018-09-28 14:16:36 +0200

    ui testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      todoList: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(result => result.json())
+      .then(json => {
+        this.setState({
+          isLoading: false,
+          todoList: json
+        });
+      });
+  }
+
   render() {
+    console.log(this.state.todoList);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.isLoading ? (
+          <div>we are loading...</div>
+        ) : (
+          this.state.todoList.map(listItem => {
+
+          })
+          <TodoListItem id={}
+        )}
       </div>
     );
   }
